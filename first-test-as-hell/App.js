e80e7c3acd30dcd4c0027a88f43d5735f1969e9d
joechesney <33613119+joechesney@users.noreply.github.com>
2018-04-02 18:36:36 -0500

    got first app working. type "npm start" to start it.

---
+++ first-test-as-hell/App.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Chupstown</Text>
+        <TextInput/>
+        <Text>Jabroni Face</Text>
+        <Text>Jabroni Face</Text>
+        <Text>Jabroni Face</Text>
+        <Text>Jabroni Face</Text>
+        <Text>Jabroni Face</Text>
+        <Text>Jabroni Face</Text>
+        <Text>Jabroni Face</Text>
+        <Text>Jabroni Face</Text>
+       
+        <Text>Jabroni Face</Text>
+        <Text>Jabroni Face</Text>
+        <Text>Jabroni Face</Text>
+        <Text>Jabroni Face</Text>
+        <Text>Jabroni Face</Text>
+        <Text>Jabroni Face</Text>
+        <Text>Jabroni Face</Text>
+        <Text>Jabroni Face</Text>
+      </View>
+      
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});

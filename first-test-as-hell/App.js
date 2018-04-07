3ade312bcf9cc4400d75228f1f18bfd289a8a993
joechesney <33613119+joechesney@users.noreply.github.com>
2018-04-07 09:44:45 -0500

    will check back later

--- first-test-as-hell/App.js
+++ first-test-as-hell/App.js
@@ -1,32 +1,31 @@
 import React from 'react';
-import { StyleSheet, Text, View, TextInput } from 'react-native';
+import { Appegistry, Image, StyleSheet, Text, View, TextInput } from 'react-native';
 
 export default class App extends React.Component {
   render() {
+    let pic = {
+      uri: 'http://lorempixel.com/400/200/cats/'
+    };
     return (
       <View style={styles.container}>
         <Text>Chupstown</Text>
         <TextInput/>
         <Text>Jabroni Face</Text>
-        <Text>Jabroni Face</Text>
-        <Text>Jabroni Face</Text>
-        <Text>Jabroni Face</Text>
-        <Text>Jabroni Face</Text>
-        <Text>Jabroni Face</Text>
-        <Text>Jabroni Face</Text>
-        <Text>Jabroni Face</Text>
-       
-        <Text>Jabroni Face</Text>
-        <Text>Jabroni Face</Text>
-        <Text>Jabroni Face</Text>
-        <Text>Jabroni Face</Text>
-        <Text>Jabroni Face</Text>
-        <Text>Jabroni Face</Text>
-        <Text>Jabroni Face</Text>
-        <Text>Jabroni Face</Text>
+        <Image source={pic} style={{width: 400, height: 200}}/>
+        <Chups skriz="Super Ultra Chupssseezzzz!" pic="http://lorempixel.com/400/200/animals/"></Chups>
       </View>
       
     );
+  }
+}
+class Chups extends React.Component {
+  render() {
+    return (
+      <View>
+        <Text> {this.props.skriz}</Text>
+        <Image source={{uri: this.props.pic}} style={{width: 400, height: 200}}/>
+      </View>
+    )
   }
 }
 
